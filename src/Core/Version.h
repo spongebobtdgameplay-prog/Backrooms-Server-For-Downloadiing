59c817ab5ca5b1a61ad11a063cb4fd2f94801c17
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-09-02 21:33:41 -0400

    Add centralized build version and update manifest URL

---
+++ src/Core/Version.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace BuildVersion
+{
+    inline constexpr const char* Text = "0.3.5";
+
+    inline constexpr const wchar_t* UpdateManifestUrl =
+        L"https://raw.githubusercontent.com/"
+        L"spongebobtdgameplay-prog/"
+        L"Backrooms-Server-For-Downloadiing/"
+        L"main/update/latest.txt";
+}
