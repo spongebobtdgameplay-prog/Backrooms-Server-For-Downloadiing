d0024caa0d9855227dd22b74ae3a2d8421cf0b3e
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-09-05 10:02:30 -0400

    Bump clickable menu fix to V0.3.15

--- src/Core/Version.h
+++ src/Core/Version.h
@@ -2,7 +2,7 @@
 
 namespace BuildVersion
 {
-    inline constexpr const char* Text = "0.3.14";
+    inline constexpr const char* Text = "0.3.15";
 
     inline constexpr const wchar_t* UpdateManifestUrl =
         L"https://raw.githubusercontent.com/"
